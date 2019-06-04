27fa2a17e2ced142972d35816e9f24f7a6e90ac4
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-06-04 11:19:27 -0400

    burger card fix

--- assets/javacript/main.js
+++ assets/javacript/main.js
@@ -1,45 +1,15 @@
 $(document).ready(() => {
 
-    // var numberOfItems = $(".component-area .component").length;
-    // var limitPerPage = 5;
-    // var totalPages = Math.round(numberOfItems / limitPerPage);
-    // var currentPage =1;
-
-    // //Limit the number of components that will be displayed per page
-    // $(".component-area .component").slice(Math.ceil(limitPerPage)).hide();
-
-    // console.log(totalPages);
-    // //add the # of Pages
-    // for (var i = 1; i <= totalPages; i++) {
-    //     //Append a componnet area for each page that exists.
-    //     $("#compContainer").append(` <div class="component-area"></div>`);
-    //     page++;
-    // }
-
-    // //When you click on an arrow, go to the next page
-    // $(".right-arrow").on("click", () => {
-    //    alert("arrow go to right");
-
-    //     var grandTotal = limitPerPage * currentPage;
-
-    //     for (let i = 0; i < grandTotal; i++) {
-    //         $(".component-area .component").eq(i).show();
-    //     }
-
-    // });
-
-
+    const card = $(".portfolio-component");
+    const langArea = $(".languages-used");
     // --------------------Smooth Scroll
-
-    var scrollLink = $(".scroll");
-
     var header = $(".header nav");
-  
-    $(window).scroll(function() {    
+
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
             header.addClass("scrolled");
-          $(".header nav ul.ul-flex li a").css("color", "#fff");
+            $(".header nav ul.ul-flex li a").css("color", "#fff");
         } else {
             header.removeClass("scrolled");
             $(".header nav ul.ul-flex li a").css("color", "#000");
@@ -53,20 +23,24 @@ $(document).ready(() => {
 
         var href = event.target.hash;
 
-        console.log( $(this.hash));
+        console.log($(this.hash));
         console.log(href);
 
-
         $("body, html").animate({
-            scrollTop: $(href).offset().top - 75
-        }, 1000);
-
-
-        // var linkHref = $(this).attr('href');
-
-        // $('html, body').animate({
-        //     scrollTop: $(linkHref).offset().top
-        // }, 850);
+            scrollTop: $(href).offset().top - 75}, 1000);
     });
-});
 
+    //add a hover arrow to cards whose height is passed 60px
+ langArea.each((int, element) => {
+    // console.log(element);
+    console.log($(this).height());
+ })
+
+    function expandEffect() {
+        const thisLang = $(this);
+        console.log(thisLang);
+        console.log("Ready!! ");
+    }
+    //when you have loaded everything check the cards to see if their height is over 60px;
+    $(document).on("load", ".languages-used", expandEffect);
+});
