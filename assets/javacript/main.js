bfe3a64aec74797d3f4dbdffc7c2caadfbcc9673
Jorge Sierra <3d.jorgesierra@gmail.com>
2019-06-06 21:46:12 -0400

    hover effects with arrow and color change implemented

--- assets/javacript/main.js
+++ assets/javacript/main.js
@@ -31,48 +31,44 @@ $(document).ready(() => {
             scrollTop: $(href).offset().top - 75
         }, 1000);
     });
+    // End of smooth scroll ------------------------------
 
+    //start card hover animations -----------------------------------
     //iterate through language area class and find the ones who exceed the height limit
     langArea.each((iteration, item) => {
-
-        const originalHeight = $(item).height();
         const langDiv = $(item);
-
         // console.log("This is the height: ")
         if (langDiv.height() > 60) {
             langDiv.css("height", "60px");
             createExpandArrow(langDiv);
         }
-
-
-
     });
 
     function createExpandArrow(element) {
         const hoverDiv = $(`<div class="hover-more"></div>`);
         const arrow = $(`<span class="downArrow"><i class="fas fa-chevron-down"></i></span>`);
-
         hoverDiv.append(arrow);
         element.append(hoverDiv);
     }
 
     function expandEffect(thisDiv) {
-
-
+        //grab the .language-uses current height t
         var curHeight = thisDiv.parent().height();
+        //change the paretn's height so that it has it's natural height
         thisDiv.parent().css("height", "auto");
+        //store the natural height number
         var autoHeight = thisDiv.parent().height();
-
+        //emulate the height change.
         thisDiv.parent().height(curHeight).animate({
             height: autoHeight
-        }, 1000);
+        }, 750);
 
     }
 
     function contractEffect(thisDiv) {
         thisDiv.parent().animate({
             height: "60px"
-        }, 1000);
+        }, 750);
     }
 
 
@@ -83,11 +79,9 @@ $(document).ready(() => {
             console.log($(this).parent().height());
             if ($(this).parent().height() === 60) {
                 expandEffect($(this))
-
             } else if (($(this).parent().height() > 60)) {
                 contractEffect($(this));
             }
-
         })
 
 });
